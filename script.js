9645364c90073c6c5fbe983e697375fa747a33b5
WeiHanNP <S10228059@connect.np.edu.sg>
2024-11-28 20:18:36 +0800

    homepage - add background blur when hovering the introduction section using javascript

--- script.js
+++ script.js
@@ -1,42 +1,73 @@
 // navagation bar below
+// Select the element with the class "hamburger" and assign it to the variable mobileNav
 const mobileNav = document.querySelector(".hamburger");
+
+// Select the element with the class "menubar" and assign it to the variable navbar
 const navbar = document.querySelector(".menubar");
 
+// Define a function named toggleNav
 const toggleNav = () => {
+  // Toggle the "active" class on the navbar element
   navbar.classList.toggle("active");
+  // Toggle the "hamburger-active" class on the mobileNav element
   mobileNav.classList.toggle("hamburger-active");
 };
+// Add a click event listener to the mobileNav element
+// When the mobileNav element is clicked, the toggleNav function will be executed
 mobileNav.addEventListener("click", () => toggleNav());
 // nav bar on top
 
+
+var movableHeader = document.getElementById("idx-header-h1");
+window.addEventListener("scroll", () => {
+  if (window.scrollY > window.innerHeight / 0.80) {
+    movableHeader.style.display = "none";
+  } else {
+    movableHeader.style.display = "block";
+  }
+});
+
 //slideshows below
-let slideIndex = 1;
-showSlides(slideIndex);
+var slideshows = document.querySelectorAll('.mySlides'); // Get all mySlides
+let slideIndex = 0;// Set the slide index to 0
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
+const showSlides = () => { // Function to change the slide
+  for (let i = 0; i < slideshows.length; i++) {// Loop through all the slides
+    slideshows[i].style.display = "none";// Hide all the slides
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+  slideIndex++;// Increase the slide index by 1
+  if (slideIndex > slideshows.length) {
+    slideIndex = 1;// Set the slide index to 1 if it's greater than the amount of slides
   }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-}
+  slideshows[slideIndex - 1].style.display = "block";// Display the current slide
+  setTimeout(showSlides, 4000); // Change image every 4 seconds
+};
+
+showSlides();
+
+
+
+
 //slideshows above
+//homepage below
+
+function blurbackground(){
+  const blurDiv = document.querySelector(".blur");
+
+  blurDiv.addEventListener("mouseover", () => {
+    document.body.style.backdropFilter = "blur(7px)";
+  });
+
+  blurDiv.addEventListener("mouseout", () => {
+    document.body.style.backdropFilter = "none";
+  });
+}
+  
+  
+
+
+
+
 
 //album below
 function albumPage(){
@@ -56,3 +87,5 @@ function changeAlbum(n){
   }
   album[n].style.display = "block";  
 }
+
+
